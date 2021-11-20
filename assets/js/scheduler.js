7bd54826e9fbf9fac00f0699a073a49b9210301c
ArtC <castillejaarturo@gmail.com>
2021-11-20 02:17:13 -0500

    added current day

---
+++ assets/js/scheduler.js
@@ -0,0 +1,15 @@
+//adding current date to Hero using Moment.js
+
+var today = moment().format('dddd, MMMM Do YYYY');
+
+$("#currentDay").text(today);
+console.log(today);
+
+//work day time blocks
+
+
+
+
+
+
+
