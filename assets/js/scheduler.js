1aed7fe3cfffc811ec451bd9bf24d3a64eb04915
ArtC <castillejaarturo@gmail.com>
2021-11-21 00:44:26 -0500

    time blocks

--- assets/js/scheduler.js
+++ assets/js/scheduler.js
@@ -1,3 +1,4 @@
+
 //adding current date to Hero using Moment.js
 
 var today = moment().format('dddd, MMMM Do YYYY');
@@ -5,8 +6,48 @@ var today = moment().format('dddd, MMMM Do YYYY');
 $("#currentDay").text(today);
 console.log(today);
 
-//work day time blocks
+//current time
+var current_time = moment().format("hh:mm a");
+console.log(current_time);
 
+//work day time blocks
+var workDay = [
+    { time: "8 am", event:""},
+    { time: "9 am", event:""},
+    { time: "10 am", event:""},
+    { time: "11 am", event:""},
+    { time: "12 pm", event:""},
+    { time: "1 pm", event:""},
+    { time: "2 pm", event:""},
+    { time: "3 pm", event:""},
+    { time: "4 pm", event:""},
+    { time: "5 pm", event:""},
+];
+console.log(workDay);
+
+function currentColor(timeLabel) {
+    var planNow = moment(current_time, "hh:mm a");
+    var planEntry = moment(timeLabel, "hh:mm a");
+    if (planNow.isBefore(planEntry) === true){
+        return "future";
+    }else if (planNow.isAfter(planEntry) === true) {
+        return "past";
+    }else {
+        return "present";
+    }
+
+}
+//rows for time blocks
+
+
+workDay.forEach(function(workDay, index) {
+    var timeLabel = workDay.time;
+    var colorTime = currentColor(timeLabel);
+    var row = '<div class="time-block" id=" ' + index + ' "><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour jsutify-content-sm-end pr-3 pt-3">'+ timeLabel + '</div><textarea class"form-control '+ colorTime +'">' 
+    + workDay.event + '</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save></i></button></div></div></div>';
+$(".container").append(row);
+console.log(colorTime);
+});
 
 
 
